617501fbb6c65a6452adaa1cd0e60282d273159d
santosh <santosh.ece06@gmail.com>
2016-12-13 12:44:21 +0530

    context specific menu and simulation home

--- app/src/main/java/com/affixus/base/adapter/SimulationHomeAdapter.java
+++ app/src/main/java/com/affixus/base/adapter/SimulationHomeAdapter.java
@@ -1,12 +1,15 @@
 package com.affixus.base.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Filter;
 import android.widget.Filterable;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.affixus.base.R;
 import com.affixus.base.modals.SamplePojo;
@@ -17,7 +20,8 @@ import java.util.List;
 /**
  * Created by santosh on 12/9/16.
  */
-public class SimulationHomeAdapter extends RecyclerView.Adapter<SimulationHomeAdapter.SimulationHomeAdapterHolder> {
+public class SimulationHomeAdapter extends RecyclerView.Adapter<SimulationHomeAdapter.SimulationHomeAdapterHolder>{
+    private static final String TAG = SimulationHomeAdapter.class.getSimpleName();
 
     private List<SamplePojo> dataList;
 
@@ -46,14 +50,39 @@ public class SimulationHomeAdapter extends RecyclerView.Adapter<SimulationHomeAd
     }
 
 
+
+
     /** View Holder Class **/
     public class SimulationHomeAdapterHolder extends RecyclerView.ViewHolder {
         TextView title;
+        ImageView imageView;
 
 
-        public SimulationHomeAdapterHolder(View itemView) {
+        public SimulationHomeAdapterHolder(final View itemView) {
             super(itemView);
+
             title = (TextView) itemView.findViewById(R.id.tvTitle);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+
+
+            /** implement click **/
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    Toast.makeText(itemView.getContext(), dataList.get(position).getDescr(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    Toast.makeText(itemView.getContext(), "Image View", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
         }
     }
 
